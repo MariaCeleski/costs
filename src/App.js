2334440f9546fe417eb4fe4a9f56a5f525e532f4
Maria de Lourdes Celeski <marialceleski@gmail.com>
2022-04-03 12:51:01 -0300

    Include JS/CSS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+//Pages
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
+
+//Layout
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Projects from "./components/pages/Projects";
+import Project from "./components/pages/Project";
+
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
+    <Router>
+      <Navbar />
+      <Container customClass="min-height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/project/:id" element={<Project />} />
+        </Routes>
+      </Container>
+      <Footer />
+    </Router>
   );
 }
 
